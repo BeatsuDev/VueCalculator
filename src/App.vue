e56bc21d66c05a3856f67a97fea7719a88d60bb1
Eric B <ericbieszczad@gmail.com>
2023-02-13 11:09:12 +0100

    Added semicolons at end of lines

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
@@ -12,9 +12,41 @@ import { RouterLink, RouterView } from 'vue-router'
     </div>
   </header>
 
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
+header {
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  width: 100vw;
+  height: 50px;
+  background-color: var(--vt-c-vue-green);
+}
 
+nav > * {
+  line-height: 50px;
+  height: 100%;
+  margin-left: 20px;
+  color: #555;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: none;
+  transition-duration: 80ms;
+}
+
+nav > *:hover {
+  color: white;
+}
+
+main {
+  max-width: 1280px;
+  margin: auto;
+  margin-top: calc(50px + 20px); /* 50px for header */
+  display: flex;
+  justify-content: center;
+}
 </style>
--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-Calculator 
+  <div class="wrapper">
+    <h1>Test out my simple calculator!</h1>
+  </div>
 </template>
 
 <style scoped>
