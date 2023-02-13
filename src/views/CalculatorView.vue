b60e203387ed18945f37a71d446194c624fee2c3
Eric B <ericbieszczad@gmail.com>
2023-02-13 09:12:58 +0100

    Added files

---
+++ src/views/CalculatorView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+Calculator 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/FeedbackView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+Feedback
+</template>
+
+<style scoped>
+
+</style>
